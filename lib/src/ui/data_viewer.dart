0f7fabda99e0d2407d1cb91498cec45a64e089c4
MuhammedKasujja <al.kasmud.2@gmail.com>
2025-07-23 19:37:56 +0300

    feat: manually trigger data syncronization

--- lib/src/ui/data_viewer.dart
+++ lib/src/ui/data_viewer.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_offline_sync/src/data/services/sync_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SyncDataViewer extends StatelessWidget {
+import 'package:flutter_offline_sync/src/data/services/sync_repository.dart';
+import 'package:flutter_offline_sync/src/manual_sync_manager.dart';
+
+class SyncDataViewer extends ConsumerWidget {
   const SyncDataViewer({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: FutureBuilder<List<Map<String, dynamic>>>(
-          future: SyncRepositoryImp().getPendingLocalUpdates(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (snapshot.data!.isEmpty) {
-                return Center(child: Text('No local updates available'));
+  Widget build(BuildContext context, ref) {
+    final syncManager = ref.watch(manualSyncManagerProvider);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: FutureBuilder<List<Map<String, dynamic>>>(
+            future: SyncRepositoryImp().getPendingLocalUpdates(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data!.isEmpty) {
+                  return Center(child: Text('No local updates available'));
+                }
+                return _jsonViewer(snapshot.data);
               }
-              return _jsonViewer(snapshot.data);
-            }
-            return Center(child: CircularProgressIndicator());
-          },
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:
+            syncManager.isLoading
+                ? null
+                : () => ref.read(manualSyncManagerProvider.notifier).syncData(),
+        child: Icon(Icons.sync),
       ),
     );
   }
